96cde2a1f9a13ed337ced868c87265a562844bdd
Johannes Wachter <johannes.wachter@massiveart.com>
2017-06-09 17:35:35 +0200

    fixed things

--- src/AppBundle/Entity/Project.php
+++ src/AppBundle/Entity/Project.php
@@ -51,4 +51,50 @@ class Project
     {
         return $this->title;
     }
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $packages;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->packages = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add package
+     *
+     * @param \AppBundle\Entity\Package $package
+     *
+     * @return Project
+     */
+    public function addPackage(\AppBundle\Entity\Package $package)
+    {
+        $this->packages[] = $package;
+
+        return $this;
+    }
+
+    /**
+     * Remove package
+     *
+     * @param \AppBundle\Entity\Package $package
+     */
+    public function removePackage(\AppBundle\Entity\Package $package)
+    {
+        $this->packages->removeElement($package);
+    }
+
+    /**
+     * Get packages
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getPackages()
+    {
+        return $this->packages;
+    }
 }
